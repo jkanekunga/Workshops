516ab7d2665d8e7385af7f656a823b83f42f7f5d
TLM-User <jkanekunga@gmail.com>
2016-03-27 00:21:40 +1000

    update function, add error checking

--- PycharmProjects/workshopPart1/hello.py
+++ PycharmProjects/workshopPart1/hello.py
@@ -1,2 +1,15 @@
 __author__ = 'John'
 print('hello world')
+
+def main():
+
+    area = calculate_area()
+    print("The are is ", area)
+
+def calculate_area():
+    l = int(input("Enter lenght:"))
+    w = int(input("Enter width:"))
+
+    return l*w
+
+main()
